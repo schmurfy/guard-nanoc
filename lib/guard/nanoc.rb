9c96cd00c8f14d2fc9f3fea630b56f0bbe6b87b1
Yann Lugrin <yann.lugrin@sans-savoir.net>
2010-10-28 09:58:39 +0200

    guard actions

--- lib/guard/nanoc.rb
+++ lib/guard/nanoc.rb
@@ -7,5 +7,31 @@ module Guard
 
     autoload :Runner, 'guard/nanoc/runner'
 
+    def initialize(watchers = [], options = {})
+      super
+
+      @runner = Runner.new(options)
+    end
+
+    def start
+      true
+    end
+
+    def stop
+      true
+    end
+
+    def reload
+      true
+    end
+
+    def run_all
+      @runner.run
+    end
+
+    def run_on_change(paths = [])
+      @runner.run
+    end
+
   end
 end
--- spec/guard/nanoc_spec.rb
+++ spec/guard/nanoc_spec.rb
@@ -2,5 +2,65 @@
 require 'spec_helper'
 
 describe Guard::Nanoc do
+  subject { Guard::Nanoc }
+
+  before(:each) do
+    @runner = Guard::Nanoc::Runner.new
+    @runner.stub!(:run).and_return(true)
+    Guard::Nanoc::Runner.stub!(:new).and_return(@runner)
+  end
+
+  context 'initialization' do
+    
+    it 'should initialize runner' do
+      Guard::Nanoc::Runner.should_receive(:new).with({:test => true}).and_return(@runner)
+      subject.new([], :test => true)
+    end
+
+  end
+
+  context 'start' do
+
+    it 'should return true' do
+      subject.new.start.should be_true
+    end
+
+  end
+
+  context 'stop' do
+
+    it 'should return true' do
+      subject.new.stop.should be_true
+    end
+
+  end
+
+  context 'reload' do
+
+    it 'should return true' do
+      subject.new.reload.should be_true
+    end
+
+  end
+
+  context 'run_all' do
+
+    it 'should call runner' do
+      @runner.should_receive(:run).and_return(true)
+      guard = subject.new
+      guard.run_on_change(['path/file.txt']).should be_true
+    end
+
+  end
+
+  context 'run_on_change' do
+
+    it 'should call runner' do
+      @runner.should_receive(:run).and_return(true)
+      guard = subject.new
+      guard.run_on_change(['path/file.txt']).should be_true
+    end
+
+  end
   
 end
